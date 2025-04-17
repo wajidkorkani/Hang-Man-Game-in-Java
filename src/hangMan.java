1c193518b6110243341549644da51b5ccf838cf6
wajidkorkani <errormrcoder@gmail.com>
2025-04-17 23:05:34 +0500

    Logic has been build.

--- src/hangMan.java
+++ src/hangMan.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 public class hangMan{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        int chance = 5;
+        int number = 20;
+        while(chance > 0){
+            System.out.print(chance < 5 ? "Try again:" : "Guess the number:");
+            int answer = input.nextInt();
+            if(number == answer){
+                System.out.println("Right answer");
+                System.out.println("You won");
+                chance = 0;
+            }else if(answer != number && chance < 0){
+                System.out.println("You lost!");
+                chance = 0;
+            }else{
+                System.out.println("Wrong answer");
+            }
+            chance --;
+        }
     }
 }
